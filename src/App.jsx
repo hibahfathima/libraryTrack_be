7d3b5b02886ac9dc2238aff5edcc5d541efb1647
hibahfathima <hibaf316@gmai.com>
2025-06-13 14:14:32 +0530

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 import BookList from './components/BookList'
-import { Route, Routes } from 'react-router-dom'
-import Addnew from './components/Addnew'
+
+
 import Footer from './components/Footer'
 import Screen from './components/Screen'
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+
 
 
 function Header() {
